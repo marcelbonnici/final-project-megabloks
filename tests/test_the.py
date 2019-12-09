d2b06eb1eca2da88758b04d709d30d4883ed14ca
Marcel Bonnici <marcelbonnici2020@u.northwestern.edu>
2019-12-09 14:50:59 -0600

    Made test file but has errors with simulation; will test on real robot

--- tests/test_the.py
+++ tests/test_the.py
@@ -3,7 +3,7 @@
 import unittest
 import rospy
 from sensor_msgs.msg import CameraInfo
-from sensor_msgs.msg import
+from baxter_core_msgs.msg import EndEffectorState
 
 class TestCases(unittest.TestCase):
     def __init__(self, *args):
@@ -13,23 +13,24 @@ class TestCases(unittest.TestCase):
     def test_tower(self):
         tower = rospy.get_param('/tower', None)
         self.assertIsNotNone(tower)
-    """
-    def test_camera(self):
-        self.camera_info = rospy.wait_for_message('/cameras/right_hand_camera/camera_info', CameraInfo, 2)
-        self.assertEqual(self.camera_info.width, 800)
-    """
-    def test_left_gripper(self):
-        gripper = baxter_interface.Gripper('left')
-        self.assertTrue(server.calibrate_grippers(gripper))
 
-        self.left = rospy.wait_for_message('/robot/end_effector/left_gripper/state', CameraInfo, 2)
+    def test_camera(self):
+        self.camera_info = rospy.wait_for_message('/cameras/right_hand_camera/camera_info', CameraInfo, 3)
+        self.assertEqual(self.camera_info.width, 800)
+
+
+    def test_left_gripper(self):
+        #gripper = baxter_interface.Gripper('left')
+        #self.assertTrue(server.calibrate_grippers(gripper))
+
+        self.left = rospy.wait_for_message('/robot/end_effector/left_gripper/state', EndEffectorState, 3)
         self.assertEqual(self.left.calibrated, 1)
 
-    def test_left_gripper(self):
-        gripper = baxter_interface.Gripper('right')
-        self.assertTrue(server.calibrate_grippers(gripper))
+    def test_right_gripper(self):
+        #gripper = baxter_interface.Gripper('right')
+        #self.assertTrue(server.calibrate_grippers(gripper))
 
-        self.left = rospy.wait_for_message('/robot/end_effector/right_gripper/state', CameraInfo, 2)
+        self.right = rospy.wait_for_message('/robot/end_effector/right_gripper/state', EndEffectorState, 3)
         self.assertEqual(self.right.calibrated, 1)
 
 if __name__=='__main__':
